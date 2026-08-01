b0ecdb6edfbe45c933dd7b06e90dc5738924f9b3
cksckscks1008 <hsc9611@gmail.com>
2026-08-01 21:50:58 +0900

    임시 파일 삭제
    임시 파일 삭제

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:share_prompt/page/login_page.dart';
+//import 'package:share_prompt/page/login_page.dart';
 import 'package:share_prompt/router/router.dart';
 
 void main() {
