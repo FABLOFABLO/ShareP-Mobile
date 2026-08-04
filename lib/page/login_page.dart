dbb20f7c644beeb8b281d02facbb64209a443fea
cksckscks1008 <hsc9611@gmail.com>
2026-08-04 23:57:03 +0900

    feat: 화면클릭시 키보드 해제기능 구현
    - 화면클릭시 키보드 해제기능 구현함.

--- lib/page/login_page.dart
+++ lib/page/login_page.dart
@@ -24,105 +24,110 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: LayoutBuilder(
-          builder: (context, constraints) {
-            return SingleChildScrollView(
-              keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
-              child: ConstrainedBox(
-                constraints: BoxConstraints(minHeight: constraints.maxHeight),
-                child: IntrinsicHeight(
-                  child: AutofillGroup(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        const SizedBox(height: 65),
-                        Image.asset('assets/logo.png'),
-                        const SizedBox(height: 50),
+    return GestureDetector(
+      onTap: () {
+        FocusManager.instance.primaryFocus?.unfocus();
+      },
+      child: Scaffold(
+        body: SafeArea(
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              return SingleChildScrollView(
+                keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(minHeight: constraints.maxHeight),
+                  child: IntrinsicHeight(
+                    child: AutofillGroup(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          const SizedBox(height: 65),
+                          Image.asset('assets/logo.png'),
+                          const SizedBox(height: 50),
 
-                        IdTextField(
-                          text: '아이디',
-                          hintText: '아이디를 입력하세요',
-                          hasIcon: false,
-                          controller: _idController,
-                          onSubmitted: (_) {
-                            _passwordFocusNode.requestFocus();
-                          },
-                        ),
+                          IdTextField(
+                            text: '아이디',
+                            hintText: '아이디를 입력하세요',
+                            hasIcon: false,
+                            controller: _idController,
+                            onSubmitted: (_) {
+                              _passwordFocusNode.requestFocus();
+                            },
+                          ),
 
-                        const SizedBox(height: 20),
+                          const SizedBox(height: 20),
 
-                        IdTextField(
-                          text: '비밀번호',
-                          hintText: '비밀번호를 입력하세요',
-                          hasIcon: true,
-                          controller: _passwordController,
-                          focusNode: _passwordFocusNode,
-                          textInputAction: TextInputAction.next,
-                          onSubmitted: (_) => TextInputAction.done,
-                        ),
+                          IdTextField(
+                            text: '비밀번호',
+                            hintText: '비밀번호를 입력하세요',
+                            hasIcon: true,
+                            controller: _passwordController,
+                            focusNode: _passwordFocusNode,
+                            textInputAction: TextInputAction.next,
+                            onSubmitted: (_) => TextInputAction.done,
+                          ),
 
-                        const Spacer(),
+                          const Spacer(),
 
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const Text(
-                              '계정이 없으신가요?',
-                              style: TextStyle(
-                                color: Color(0xFF484555),
-                                fontWeight: FontWeight.w500,
-                                fontSize: 16,
-                              ),
-                            ),
-                            TextButton(
-                              onPressed: () => context.go('/sign_up'),
-                              child: const Text(
-                                '회원가입',
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Text(
+                                '계정이 없으신가요?',
                                 style: TextStyle(
-                                  color: Color(0xFF928CFF),
-                                  fontWeight: FontWeight.w700,
+                                  color: Color(0xFF484555),
+                                  fontWeight: FontWeight.w500,
                                   fontSize: 16,
                                 ),
                               ),
-                            ),
-                          ],
-                        ),
-
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 20),
-                          child: GestureDetector(
-                            onTap: () {},
-                            child: Container(
-                              width: double.infinity,
-                              height: 60,
-                              alignment: Alignment.center,
-                              decoration: BoxDecoration(
-                                color: const Color(0xFF6B4EFE),
-                                borderRadius: BorderRadius.circular(12),
+                              TextButton(
+                                onPressed: () => context.go('/sign_up'),
+                                child: const Text(
+                                  '회원가입',
+                                  style: TextStyle(
+                                    color: Color(0xFF928CFF),
+                                    fontWeight: FontWeight.w700,
+                                    fontSize: 16,
+                                  ),
+                                ),
                               ),
-                              child: const Text(
-                                '로그인하기',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 17,
-                                  fontWeight: FontWeight.w600,
+                            ],
+                          ),
+
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 20),
+                            child: GestureDetector(
+                              onTap: () {},
+                              child: Container(
+                                width: double.infinity,
+                                height: 60,
+                                alignment: Alignment.center,
+                                decoration: BoxDecoration(
+                                  color: const Color(0xFF6B4EFE),
+                                  borderRadius: BorderRadius.circular(12),
+                                ),
+                                child: const Text(
+                                  '로그인하기',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 17,
+                                    fontWeight: FontWeight.w600,
+                                  ),
                                 ),
                               ),
                             ),
                           ),
-                        ),
-                        const SizedBox(height: 20),
-                      ],
+                          const SizedBox(height: 20),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-            );
-          },
+              );
+            },
+          ),
         ),
-      ),
+      )
     );
   }
 }
--- lib/page/sign_up_page.dart
+++ lib/page/sign_up_page.dart
@@ -29,121 +29,126 @@ class _SignUpPageState extends State<SignUpPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: LayoutBuilder(
-          builder: (context, constraints) {
-            return SingleChildScrollView(
-              keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
-              child: ConstrainedBox(
-                constraints: BoxConstraints(minHeight: constraints.maxHeight),
-                child: IntrinsicHeight(
-                  child: AutofillGroup(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        const SizedBox(height: 65),
-                        Image.asset('assets/logo.png'),
-                        const SizedBox(height: 50),
+    return GestureDetector(
+      onTap: () {
+        FocusManager.instance.primaryFocus?.unfocus();
+      },
+      child: Scaffold(
+        body: SafeArea(
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              return SingleChildScrollView(
+                keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(minHeight: constraints.maxHeight),
+                  child: IntrinsicHeight(
+                    child: AutofillGroup(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          const SizedBox(height: 65),
+                          Image.asset('assets/logo.png'),
+                          const SizedBox(height: 50),
 
-                        IdTextField(
-                          text: '아이디',
-                          hintText: '아이디를 입력하세요',
-                          hasIcon: false,
-                          controller: _idController,
-                          onSubmitted: (_) {
-                            _passwordFocusNode.requestFocus();
-                          },
-                        ),
+                          IdTextField(
+                            text: '아이디',
+                            hintText: '아이디를 입력하세요',
+                            hasIcon: false,
+                            controller: _idController,
+                            onSubmitted: (_) {
+                              _passwordFocusNode.requestFocus();
+                            },
+                          ),
 
-                        const SizedBox(height: 20),
+                          const SizedBox(height: 20),
 
-                        IdTextField(
-                          text: '비밀번호',
-                          hintText: '비밀번호를 입력하세요',
-                          hasIcon: true,
-                          controller: _passwordController,
-                          focusNode: _passwordFocusNode,
-                          textInputAction: TextInputAction.next,
-                          onSubmitted: (_) {
-                            _checkPasswordFocusNode.requestFocus();
-                          },
-                        ),
+                          IdTextField(
+                            text: '비밀번호',
+                            hintText: '비밀번호를 입력하세요',
+                            hasIcon: true,
+                            controller: _passwordController,
+                            focusNode: _passwordFocusNode,
+                            textInputAction: TextInputAction.next,
+                            onSubmitted: (_) {
+                              _checkPasswordFocusNode.requestFocus();
+                            },
+                          ),
 
-                        const SizedBox(height: 20),
+                          const SizedBox(height: 20),
 
-                        IdTextField(
-                          text: '비밀번호 재확인',
-                          hintText: '동일한 비밀번호를 입력하세요',
-                          hasIcon: true,
-                          controller: _checkPasswordController,
-                          focusNode: _checkPasswordFocusNode,
-                          textInputAction: TextInputAction.done,
-                          onSubmitted: (_) {
-                            FocusScope.of(context).unfocus();
-                          },
-                        ),
+                          IdTextField(
+                            text: '비밀번호 재확인',
+                            hintText: '동일한 비밀번호를 입력하세요',
+                            hasIcon: true,
+                            controller: _checkPasswordController,
+                            focusNode: _checkPasswordFocusNode,
+                            textInputAction: TextInputAction.done,
+                            onSubmitted: (_) {
+                              FocusScope.of(context).unfocus();
+                            },
+                          ),
 
-                        const Spacer(),
+                          const Spacer(),
 
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const Text(
-                              '이미 계정이 있으신가요?',
-                              style: TextStyle(
-                                color: Color(0xFF484555),
-                                fontWeight: FontWeight.w500,
-                                fontSize: 16,
-                              ),
-                            ),
-                            TextButton(
-                              onPressed: () => context.go('/login'),
-                              child: const Text(
-                                '로그인',
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Text(
+                                '이미 계정이 있으신가요?',
                                 style: TextStyle(
-                                  color: Color(0xFF928CFF),
-                                  fontWeight: FontWeight.w700,
+                                  color: Color(0xFF484555),
+                                  fontWeight: FontWeight.w500,
                                   fontSize: 16,
                                 ),
                               ),
-                            ),
-                          ],
-                        ),
-
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 20),
-                          child: GestureDetector(
-                            onTap: () {},
-                            child: Container(
-                              width: double.infinity,
-                              height: 60,
-                              alignment: Alignment.center,
-                              decoration: BoxDecoration(
-                                color: const Color(0xFF6B4EFE),
-                                borderRadius: BorderRadius.circular(12),
+                              TextButton(
+                                onPressed: () => context.go('/login'),
+                                child: const Text(
+                                  '로그인',
+                                  style: TextStyle(
+                                    color: Color(0xFF928CFF),
+                                    fontWeight: FontWeight.w700,
+                                    fontSize: 16,
+                                  ),
+                                ),
                               ),
-                              child: const Text(
-                                '회원가입하기',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 17,
-                                  fontWeight: FontWeight.w600,
+                            ],
+                          ),
+
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 20),
+                            child: GestureDetector(
+                              onTap: () {},
+                              child: Container(
+                                width: double.infinity,
+                                height: 60,
+                                alignment: Alignment.center,
+                                decoration: BoxDecoration(
+                                  color: const Color(0xFF6B4EFE),
+                                  borderRadius: BorderRadius.circular(12),
+                                ),
+                                child: const Text(
+                                  '회원가입하기',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 17,
+                                    fontWeight: FontWeight.w600,
+                                  ),
                                 ),
                               ),
                             ),
                           ),
-                        ),
-                        const SizedBox(height: 20),
-                      ],
+                          const SizedBox(height: 20),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-            );
-          },
+              );
+            },
+          ),
         ),
-      ),
+      )
     );
   }
 }
