af2cd93cc86d79673f573644bcaa5ada2eb9f771
cksckscks1008 <hsc9611@gmail.com>
2026-08-04 22:57:04 +0900

    style: 색상, 텍스트스타일 상수설정
    - 색상, 텍스트 스타일을 피그마 디자인 시스템에서 가져와 constants폴더에 설정함

---
+++ lib/constants/app_text_style.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+
+  static const TextStyle title1 = TextStyle(
+    fontSize: 24.0,
+    fontWeight: FontWeight.w600
+  );
+
+  static const TextStyle title2 = TextStyle(
+      fontSize: 22.0,
+      fontWeight: FontWeight.w600
+  );
+
+  static const TextStyle title3 = TextStyle(
+      fontSize: 20.0,
+      fontWeight: FontWeight.w600
+  );
+
+  static const TextStyle body1 = TextStyle(
+      fontSize: 20.0,
+      fontWeight: FontWeight.w500
+  );
+
+  static const TextStyle body2 = TextStyle(
+      fontSize: 18.0,
+      fontWeight: FontWeight.w500
+  );
+
+  static const TextStyle body3 = TextStyle(
+      fontSize: 16.0,
+      fontWeight: FontWeight.w500
+  );
+
+  static const TextStyle body4 = TextStyle(
+      fontSize: 14.0,
+      fontWeight: FontWeight.w500
+  );
+
+  static const TextStyle body5 = TextStyle(
+      fontSize: 12.0,
+      fontWeight: FontWeight.w500
+  );
+
+  static const TextStyle body1Bold = TextStyle(
+      fontSize: 20.0,
+      fontWeight: FontWeight.bold
+  );
+
+  static const TextStyle body2Bold = TextStyle(
+      fontSize: 18.0,
+      fontWeight: FontWeight.bold
+  );
+
+  static const TextStyle body3Bold = TextStyle(
+      fontSize: 16.0,
+      fontWeight: FontWeight.bold
+  );
+
+  static const TextStyle body4Bold = TextStyle(
+      fontSize: 14.0,
+      fontWeight: FontWeight.bold
+  );
+
+  static const TextStyle body5Bold = TextStyle(
+      fontSize: 12.0,
+      fontWeight: FontWeight.bold
+  );
+
+  static const TextStyle caption1 = TextStyle(
+      fontSize: 18.0,
+      fontWeight: FontWeight.w400
+  );
+
+  static const TextStyle caption2 = TextStyle(
+      fontSize: 16.0,
+      fontWeight: FontWeight.w400
+  );
+
+  static const TextStyle caption3 = TextStyle(
+      fontSize: 14.0,
+      fontWeight: FontWeight.w400
+  );
+
+  static const TextStyle caption4 = TextStyle(
+      fontSize: 12.0,
+      fontWeight: FontWeight.w400
+  );
+}
