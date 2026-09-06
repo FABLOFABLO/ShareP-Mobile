1726972d93ba3a1852791f75d17ab75deb70b23a
cksckscks1008 <hsc9611@gmail.com>
2026-09-07 07:32:33 +0900

    fix: 홈화면(메인) 오류 수정
    - 프롬프트 카드들이 ListView 빌더로 쌓일때 itemcount를 더미데이터의 length로 설정하여 화면이 오버 스크롤되는 오류를 수정함.

--- lib/page/home/widget/prompt_sort_button.dart
+++ lib/page/home/widget/prompt_sort_button.dart
@@ -14,43 +14,42 @@ class _PromptSortButtonState extends State<PromptSortButton> {
   OverlayEntry? _overlayEntry;
 
   void _showOverlay() {
-    final String otherOption =
-    now == '최신순' ? '좋아요순' : '최신순';
+    final String otherOption = now == '최신순' ? '좋아요순' : '최신순';
     _overlayEntry = OverlayEntry(
-      builder: (context) =>
-          Positioned(
-            top: 100,
-            left: 27,
-            child: GestureDetector(
-              onTap: () {
-                setState(() {
-                  now = otherOption;
-                  _hideOverlay();
-                });
-              },
-              child: Container(
-                height: 36,
-                width: 88,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(30),
-                    color: AppColor.white,
-                    border: Border.all(
-                    color: AppColor.primary,
-                      width: 1.5
-                )
-
-                ),
-                child: Center(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(otherOption, style: AppTextStyles.body5.copyWith(color: AppColor.primary),),
-                    ],
+      builder: (context) => Positioned(
+        top: 100,
+        left: 27,
+        child: GestureDetector(
+          onTap: () {
+            setState(() {
+              now = otherOption;
+              _hideOverlay();
+            });
+          },
+          child: Container(
+            height: 36,
+            width: 88,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: AppColor.white,
+              border: Border.all(color: AppColor.primary, width: 1.5),
+            ),
+            child: Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    otherOption,
+                    style: AppTextStyles.body5.copyWith(
+                      color: AppColor.primary,
+                    ),
                   ),
-                ),
+                ],
               ),
-            )
+            ),
           ),
+        ),
+      ),
     );
 
     Overlay.of(context).insert(_overlayEntry!);
@@ -68,19 +67,19 @@ class _PromptSortButtonState extends State<PromptSortButton> {
         height: 36,
         width: 88,
         decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(30),
-            color: AppColor.secondary,
-            border: Border.all(
-            color: AppColor.primary,
-              width: 1.5
-        )
+          borderRadius: BorderRadius.circular(30),
+          color: AppColor.secondary,
+          border: Border.all(color: AppColor.primary, width: 1.5),
         ),
         child: Center(
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(now, style: AppTextStyles.body5.copyWith(color: AppColor.primary)),
-              Icon(Icons.keyboard_arrow_down_rounded, color: AppColor.primary)
+              Text(
+                now,
+                style: AppTextStyles.body5.copyWith(color: AppColor.primary),
+              ),
+              Icon(Icons.keyboard_arrow_down_rounded, color: AppColor.primary),
             ],
           ),
         ),
