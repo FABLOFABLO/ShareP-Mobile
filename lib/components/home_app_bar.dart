7ce14a85e9d4ee4d253e97c19de3db7f26448bfa
cksckscks1008 <hsc9611@gmail.com>
2026-09-06 18:49:24 +0900

    feat: 홈화면내 드롭다운 버튼 구현
    - OverlayEntry를 사용하여 드롭다운 버튼을 구현함.

--- lib/components/home_app_bar.dart
+++ lib/components/home_app_bar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:share_prompt/constants/app_color.dart';
 import 'package:share_prompt/constants/app_text_style.dart';
+import 'package:share_prompt/page/home/widget/prompt_sort_button.dart';
 
 class HomeAppBar extends StatefulWidget implements PreferredSizeWidget {
   const HomeAppBar({super.key});
@@ -18,32 +19,9 @@ class _HomeAppBarState extends State<HomeAppBar> {
     return Padding(
       padding: const EdgeInsets.only(left: 26.0, top: 60),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          Container(
-            width: 88,
-            height: 36,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(100),
-              color: AppColor.secondary,
-              border: Border.all(color: AppColor.primary, width: 1),
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  '최신순',
-                  style: AppTextStyles.body5.copyWith(
-                    color: AppColor.primary,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-                Icon(
-                  Icons.keyboard_arrow_down_rounded,
-                  color: AppColor.primary,
-                ),
-              ],
-            ),
-          ),
+          PromptSortButton()
         ],
       ),
     );
---
+++ lib/page/home/widget/prompt_sort_button.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:share_prompt/constants/app_color.dart';
+import 'package:share_prompt/constants/app_text_style.dart';
+
+class PromptSortButton extends StatefulWidget {
+  const PromptSortButton({super.key});
+
+  @override
+  State<PromptSortButton> createState() => _PromptSortButtonState();
+}
+
+class _PromptSortButtonState extends State<PromptSortButton> {
+  String now = '최신순';
+  OverlayEntry? _overlayEntry;
+
+  void _showOverlay() {
+    final String otherOption =
+    now == '최신순' ? '좋아요순' : '최신순';
+    _overlayEntry = OverlayEntry(
+      builder: (context) =>
+          Positioned(
+            top: 100,
+            left: 27,
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  now = otherOption;
+                  _hideOverlay();
+                });
+              },
+              child: Container(
+                height: 36,
+                width: 88,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: AppColor.primary
+                ),
+                child: Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(otherOption, style: AppTextStyles.body5.copyWith(color: AppColor.white),),
+                      Icon(Icons.keyboard_arrow_down_rounded, color: AppColor.white,)
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ),
+    );
+
+    Overlay.of(context).insert(_overlayEntry!);
+  }
+
+  void _hideOverlay() {
+    _overlayEntry!.remove();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _showOverlay,
+      child: Container(
+        height: 36,
+        width: 88,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            color: AppColor.primary
+        ),
+        child: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(now, style: AppTextStyles.body5.copyWith(color: AppColor.white)),
+              const SizedBox(width: 3,),
+              Icon(Icons.keyboard_arrow_down_rounded, color: AppColor.white)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
