b1a672c04f6c28db63612820d2e1a21681af76a7
cksckscks1008 <hsc9611@gmail.com>
2026-09-10 00:23:52 +0900

    feat: 로그아웃, 회원탈퇴 dialog 구현
    - 로그아웃 버튼에 dialog를 구현함.
    
    - 회원탈퇴 버튼에 dialog를 구현함.
    
    - 회원탈퇴의 재확인 과정에서 두개의 dialog를 동시에 pop할수있게 popUntill을 사용함.

--- lib/page/my/setting_page.dart
+++ lib/page/my/setting_page.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:share_prompt/components/default_app_bar.dart';
 import 'package:share_prompt/constants/app_color.dart';
+import 'package:share_prompt/constants/app_text_style.dart';
 import 'package:share_prompt/page/my/widget/setting_box.dart';
 
 class SettingPage extends StatelessWidget {
@@ -28,11 +30,87 @@ class SettingPage extends StatelessWidget {
                   padding: const EdgeInsets.symmetric(vertical: 38),
                   child: Column(
                     children: [
-                      SettingBox(label: '아이디 변경', onTap: () => context.push('/id')),
-                      SettingBox(label: '닉네임 변경', onTap: () => context.push('/nickname')),
-                      SettingBox(label: '비밀번호 변경', onTap: () => context.push('/password')),
-                      SettingBox(label: '로그아웃', onTap: () {}),
-                      SettingBox(label: '회원탈퇴', onTap: () {}),
+                      SettingBox(
+                        label: '아이디 변경',
+                        onTap: () => context.push('/id'),
+                      ),
+                      SettingBox(
+                        label: '닉네임 변경',
+                        onTap: () => context.push('/nickname'),
+                      ),
+                      SettingBox(
+                        label: '비밀번호 변경',
+                        onTap: () => context.push('/password'),
+                      ),
+                      SettingBox(
+                        label: '로그아웃',
+                        onTap: () {
+                          showCupertinoDialog(
+                            context: context,
+                            builder: (context) {
+                              return CupertinoAlertDialog(
+                                title: const Text('로그아웃 하시겠습니까?'),
+                                actions: [
+                                  CupertinoDialogAction(
+                                    isDestructiveAction: true,
+                                    onPressed: () => context.pop(),
+                                    child: Text(
+                                      '예',
+                                      style: AppTextStyles.body2Bold.copyWith(
+                                        color: AppColor.primary,
+                                      ),
+                                    ),
+                                  ),
+                                  CupertinoDialogAction(
+                                    onPressed: () => context.pop(),
+                                    child: Text(
+                                      '아니오',
+                                      style: AppTextStyles.body2Bold.copyWith(
+                                        color: AppColor.primary,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        },
+                      ),
+                      SettingBox(
+                        label: '회원탈퇴',
+                        onTap: () {
+                          showCupertinoDialog(
+                            context: context,
+                            builder: (context) {
+                              return CupertinoAlertDialog(
+                                title: const Text('회원탈퇴를 하시겠습니까?'),
+                                actions: [
+                                  CupertinoDialogAction(
+                                    isDestructiveAction: true,
+                                    onPressed: () =>
+                                        _showDeleteAccount(context),
+                                    child: Text(
+                                      '예',
+                                      style: AppTextStyles.body2Bold.copyWith(
+                                        color: AppColor.primary,
+                                      ),
+                                    ),
+                                  ),
+                                  CupertinoDialogAction(
+                                    onPressed: () => context.pop(),
+                                    child: Text(
+                                      '아니오',
+                                      style: AppTextStyles.body2Bold.copyWith(
+                                        color: AppColor.primary,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        },
+                      ),
                     ],
                   ),
                 ),
@@ -41,6 +119,38 @@ class SettingPage extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+
+  void _showDeleteAccount(BuildContext context) {
+    showCupertinoDialog(
+      context: context,
+      builder: (context) {
+        return CupertinoAlertDialog(
+          title: const Text('정말로 회원탈퇴를 하시겠습니까?'),
+          actions: [
+            CupertinoDialogAction(
+              isDestructiveAction: true,
+              onPressed: () => Navigator.of(context).popUntil(ModalRoute.withName('/setting')),
+              child: Text(
+                '예',
+                style: AppTextStyles.body2Bold.copyWith(
+                  color: AppColor.primary,
+                ),
+              ),
+            ),
+            CupertinoDialogAction(
+              onPressed: () => Navigator.of(context).popUntil(ModalRoute.withName('/setting')),
+              child: Text(
+                '아니오',
+                style: AppTextStyles.body2Bold.copyWith(
+                  color: AppColor.primary,
+                ),
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
